a10397e2fdaa71fa05422812235c8b666243491d
David Silva <david.silva@dokbid.com>
2018-01-04 23:07:29 -0500

    Created an endpoint to PATCH users

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -3,7 +3,15 @@ const UserModel = require('../../models/user');
 const UserSerializer = require('../../serializers').users;
 
 module.exports = {
-    update: () => {},
+    update: (req, res) => {
+        const data = req.body.data || { attributes: {}};
+        UserModel.findByIdAndUpdate(req.user.id, { $set: data.attributes }, {upsert: true})
+        .then((doc) => {
+            res.send(UserSerializer(UserModel.tidy(doc)));
+        }).catch((err) => {
+            res.status(503).send({ error: 'server error catched', err });
+        });
+    },
     findById: () => {},
     getAll: (req, res) => {
         UserModel.find().lean().then(docs => {
