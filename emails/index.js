3a42753d0011d3a69a9fe983956bcb6ba17101da
David Silva <david.silva@dokbid.com>
2018-02-17 12:40:40 -0500

    added addSubscriber function to emails file

---
+++ emails/index.js
@@ -0,0 +1,35 @@
+const Config = require('../config');
+const Mailchimp = require('mailchimp-api-v3');
+
+const mailchimp = Config.MAILCHIMP_API_KEY ? new Mailchimp(Config.MAILCHIMP_API_KEY) : {};
+
+console.log(Config);
+const addSubscriber = (email, data) => {
+    if (!email || !Config.MAILCHIMP_API_KEY || !Config.MAILCHIMP_LIST_ID) {
+        const msg = `Ignoring adding subscriber, missing params ${!email ? 'email': 'config'}`;
+        console.warn(msg);
+        return Promise.resolve({ msg });
+    }
+
+    return mailchimp.post(`lists/${Config.MAILCHIMP_LIST_ID}`, {
+        update_existing: true,
+        members: [{
+            email_address: email.toLowerCase(),
+            status : data.status || 'subscribed',
+            merge_fields: {
+                "FNAME": data.firstName,
+            },
+        }],
+    }).then(m => {
+        if (m.errors) {
+            console.log('Error adding new subscriber to MC', m.errors);
+        }
+        return m;
+    }).catch(err => {
+        console.warn('Failed adding subscriber', email, err);
+    });
+}
+
+module.exports = {
+    addSubscriber,
+}
