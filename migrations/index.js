c090e65721c77e90188971ae708da5a30944afb3
David Silva <david.silva@dokbid.com>
2018-02-27 15:56:11 -0500

    changed option to run migrations as a route

--- migrations/index.js
+++ migrations/index.js
@@ -3,23 +3,31 @@
  * - Store in the db which migrations have run yet
  * - Allow to run a specific migration by name
  * - Allow to run the down function in a migration */
-const challenges0to8 = require('./1519748712-challenges-0-to-8.js');
+const Promise = require('bluebird');
+const ccAdminUser = require('./1519748712-codecorgi-admin-user.js');
+const mongoConnection = require('../config/mongo');
+// const challenges0to8 = require('./1519748712-challenges-0-to-8.js');
 
 const migrations = [
-    challenges0to8,
+    ccAdminUser,
+    // challenges0to8,
 ];
 
-const run = (dir) => {
-    let count = 0;
+const run = (dir, res) => {
+    const promises = [];
     migrations.forEach(m => {
-        m[dir]();
-        count++;
+        promises.push(m[dir]());
     });
-    return `${count} Migrations ran successfully`;
+    return Promise.all(promises).then(() => {
+        return res.send({ msg: `${promises.length} Migrations ran successfully`});
+    }).catch((e) => {
+        console.error('Failed to create admin user');
+        res.send({ error: e });
+    });
 }
 
-const up = () => {
-    return run('up');
+const up = (req, res) => {
+    run('up', res);
 }
 
 const down = () => {
